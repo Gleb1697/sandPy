81901405b7a1e54f82121c1aadd70240dde9f288
Tolyanskyman <anewmantheweapon@gmail.com>
2021-11-05 17:38:13 +0300

    very good(class version) yes it is version with class, i am writing this

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import pygame
 from pygame import KEYDOWN
 from math import pi
+import sys
  
 # Initialize the game engine
 pygame.init()
@@ -22,44 +23,51 @@ rectx = (SIZEX-rectsizex)/2
 recty = (SIZEY-rectsizey)/2 
 # Set the height and width of the screen
 SIZE = [SIZEX, SIZEY]
-screen = pygame.display.set_mode(SIZE)
+
+class main():
+    
+    def __init__(self):
+        self.screen = pygame.display.set_mode(SIZE)
+        pygame.display.set_caption("Example code for the draw module")
  
-pygame.display.set_caption("Example code for the draw module")
- 
-#Loop until the user clicks the close button.
-done = False
-clock = pygame.time.Clock()
- 
-while not done:
- 
-    # This limits the while loop to a max of 10 times per second.
-    # Leave this out and we will use all CPU we can.
-    clock.tick(100)
-     
-    for event in pygame.event.get(): # User did something
-        if event.type == pygame.QUIT: # If user clicked close
-            done=True
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                rectx -=movex
-            if event.key == pygame.K_RIGHT:
-                rectx +=movex
-            if event.key == pygame.K_UP:
-                recty -=movey
-            if event.key == pygame.K_DOWN:
-                recty +=movey 
+        #Loop until the user clicks the close button.
+        done = False
+        self.clock = pygame.time.Clock()
+        # This limits the while loop to a max of 10 times per second.
+        # Leave this out and we will use all CPU we can.
+        self.clock.tick(100)
+    
+    def run(self): 
+        global rectx
+        global recty
+        global rectsizex
+        global rectsizey
+        while True:
+            for event in pygame.event.get(): # User did something
+                if event.type == pygame.QUIT: # If user clicked close
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_LEFT:
+                        rectx -=movex
+                    if event.key == pygame.K_RIGHT:
+                        rectx +=movex
+                    if event.key == pygame.K_UP:
+                        recty -=movey
+                    if event.key == pygame.K_DOWN:
+                        recty +=movey 
             # Flag that we are done so we exit this loop
  
     # All drawing code happens after the for loop and but
     # inside the main while done==False loop.
      
     # Clear the screen and set the screen background
-    screen.fill(WHITE)
-    rect = [rectx, recty, rectsizex, rectsizey]
+            self.screen.fill(WHITE)
+            self.rect = [rectx, recty, rectsizex, rectsizey]
     # Draw a solid rectangle
-    pygame.draw.rect(screen, BLACK, rect)
+            pygame.draw.rect(self.screen, BLACK, self.rect)
 
-    pygame.display.flip()
+            pygame.display.flip()
  
 # Be IDLE friendly
-pygame.quit()
+main().run()
