76c1585f990b130d946d8d19a5a6aed7425a93f0
Alexander Yashin <alexandr.yashin@emlid.com>
2021-11-12 19:07:48 +0300

    refactor main.py

--- main.py
+++ main.py
@@ -1,68 +1,54 @@
-# Import a library of functions called 'pygame'
-import pygame
-from pygame import KEYDOWN
-from math import pi
 import sys
- 
-# Initialize the game engine
-pygame.init()
- 
-# Define the colors we will use in RGB format
+import pygame
+
 BLACK = (  0,   0,   0)
 WHITE = (255, 255, 255)
 BLUE =  (  0,   0, 255)
 GREEN = (  0, 255,   0)
 RED =   (255,   0,   0)
-SIZEX = 400
-SIZEY = 300
-# Set the height and width of the screen
-SIZE = [SIZEX, SIZEY]
+SIZEX = 800
+SIZEY = 600
+SIZE = (SIZEX, SIZEY)
 
-class main():
-    movex = 10
-    movey = 10
-    rectsizex = SIZEX/6
-    rectsizey = SIZEY/20
-    rectx = (SIZEX-rectsizex)/2
-    recty = (SIZEY-rectsizey)/2 
+
+pygame.init()
+
+
+class Game():
     def __init__(self):
+        self.movex = 10
+        self.movey = 10
+        self.rectsizex = 30
+        self.rectsizey = 30
+        self.rectx = (SIZEX - self.rectsizex) / 2
+        self.recty = (SIZEY - self.rectsizey) / 2
         self.screen = pygame.display.set_mode(SIZE)
-        pygame.display.set_caption("Example code for the draw module")
- 
-        #Loop until the user clicks the close button.
-        done = False
+        pygame.display.set_caption("Game")
+
         self.clock = pygame.time.Clock()
-        # This limits the while loop to a max of 10 times per second.
-        # Leave this out and we will use all CPU we can.
-        self.clock.tick(100)
-    
-    def run(self): 
+
+    def run(self):
         while True:
-            for event in pygame.event.get(): # User did something
-                if event.type == pygame.QUIT: # If user clicked close
+            self.clock.tick(60)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_LEFT:
-                        self.rectx -=self.movex
-                    if event.key == pygame.K_RIGHT:
-                        self.rectx +=self.movex
-                    if event.key == pygame.K_UP:
-                        self.recty -=self.movey
-                    if event.key == pygame.K_DOWN:
-                        self.recty +=self.movey 
-            # Flag that we are done so we exit this loop
- 
-    # All drawing code happens after the for loop and but
-    # inside the main while done==False loop.
-     
-    # Clear the screen and set the screen background
+
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_LEFT]:
+                self.rectx -= self.movex
+            if keys[pygame.K_RIGHT] :
+                self.rectx += self.movex
+            if keys[pygame.K_UP]:
+                self.recty -= self.movey
+            if keys[pygame.K_DOWN]:
+                self.recty += self.movey
+
             self.screen.fill(WHITE)
             self.rect = [self.rectx, self.recty, self.rectsizex, self.rectsizey]
-    # Draw a solid rectangle
             pygame.draw.rect(self.screen, BLACK, self.rect)
-
             pygame.display.flip()
- 
-# Be IDLE friendly
-main().run()
+
+
+Game().run()
